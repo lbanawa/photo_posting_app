592b0ed76e8a2554a58583dad0e931322bc4f742
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-05-05 13:07:49 -0400

    retrieve data from firestore

--- PhotoPostingApp/FeedViewController.swift
+++ PhotoPostingApp/FeedViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
+    
+    var userEmailArray = [String]()
+    var userCaptionArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,19 +25,64 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
 
         // Do any additional setup after loading the view.
+        
+        getDataFromFirestore()
+    }
+    
+    func getDataFromFirestore() {
+        
+        let firestoreDatabase = Firestore.firestore()
+        
+        // Attaches a listener for QuerySnapshot events -- listens for changes made to data
+        firestoreDatabase.collection("Posts").addSnapshotListener { (snapshot, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    // find documents with snapshot -- snapshot.documents gives an array of documents in the Posts collection
+                    for document in snapshot!.documents {
+                        let documentID = document.documentID
+                        
+                        if let postedBy = document.get("postedBy") as? String {
+                            self.userEmailArray.append(postedBy)
+                        }
+                        
+                        if let postComment = document.get("postComment") as? String {
+                            self.userCaptionArray.append(postComment)
+                        }
+                        
+                        if let likes = document.get("likes") as? Int {
+                            self.likeArray.append(likes)
+                        }
+                        
+                        if let imageUrl = document.get("imageURL") as? String {
+                            self.userImageArray.append(imageUrl)
+                        }
+                        
+                    }
+                    
+                    self.tableView.reloadData()
+                    
+                }
+                
+                
+            }
+        }
+        
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userEmailArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // connect prototype cell to TableView
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.usernameLabel.text = "test@test.com"
-        cell.likeLabel.text = "0"
-        cell.captionLabel.text = "caption"
+        cell.usernameLabel.text = userEmailArray[indexPath.row]
+        cell.likeLabel.text = String(likeArray[indexPath.row])
+        cell.captionLabel.text = userCaptionArray[indexPath.row]
         cell.userImageView.image = UIImage(named: "selectimage1.png")
         return cell
     }
